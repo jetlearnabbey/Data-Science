4fe5149f281d9c6bc35002f417dda2e1997f9faa
jetlearnabbey <abbey.edvard@gmail.com>
2025-05-16 15:56:40 +0100

    Update machine.py

--- machine.py
+++ machine.py
@@ -1,10 +1,15 @@
 import pandas as pd
-
+from sklearn.impute import SimpleImputer
 import numpy as np
 
 dataset = pd.read_csv("Data1.csv")
 
 print(dataset.info())
 
-#splitting int0 input and target
+#splitting into input and target
+#print(dataset.iloc[:,1:3])
+
+imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
+dataset.iloc[:,1:3]= imputer.fit_transform(dataset.iloc[:,1:3])
+print(dataset.iloc[:,1:3])
 
