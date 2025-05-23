cb07ffb3aec938f6e930807263e48609aa76456c
jetlearnabbey <abbey.edvard@gmail.com>
2025-05-23 15:53:32 +0100

    Update machine.py

--- machine.py
+++ machine.py
@@ -1,6 +1,10 @@
 import pandas as pd
-from sklearn.impute import SimpleImputer
 import numpy as np
+
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 
 dataset = pd.read_csv("Data1.csv")
 
@@ -12,4 +16,11 @@ print(dataset.info())
 imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
 dataset.iloc[:,1:3]= imputer.fit_transform(dataset.iloc[:,1:3])
 print(dataset.iloc[:,1:3])
+
+ct = ColumnTransformer(transformers = [("encoder",OneHotEncoder(),[0])],remainder="passthrough")
+dataset = ct.fit_transform(dataset)
+print(dataset)
+
+
+
 
